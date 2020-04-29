c1e5f2d77c0630368cfd737bb0ef635d166757d7
fotcorn <fotcorn@gmail.com>
2020-04-29 23:34:28 +0100

    resolution and aspect ration independent rendering setup

--- CableCar/CableCar.cpp
+++ CableCar/CableCar.cpp
@@ -4,20 +4,48 @@
 
 #include "Texture.h"
 
+constexpr int VIRTUAL_WIDTH = 1920;
+constexpr int VIRTUAL_HEIGHT = 1080;
+constexpr float VIRTUAL_ASPECT_RATIO = static_cast<float>(VIRTUAL_WIDTH) / static_cast<float>(VIRTUAL_HEIGHT);
+
 int main(int argc, char* args[]) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cerr << "Failed to initialize video: " << SDL_GetError() << std::endl;
         return 1;
     }
 
-    SDL_Window* window =
-        SDL_CreateWindow("CableCar", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1024, 786, SDL_WINDOW_SHOWN);
+    // weird resolution to test aspect ratio and resolution independent rendering
+    int screenWidth = 1700;
+    int screenHeight = 900;
+
+    SDL_Window* window = SDL_CreateWindow("CableCar", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth,
+                                          screenHeight, SDL_WINDOW_SHOWN);
     if (window == nullptr) {
         std::cerr << "Failed to create window: " << SDL_GetError();
         return 1;
     }
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, 4, SDL_RENDERER_ACCELERATED);
+
+    // based on http://www.david-amador.com/2013/04/opengl-2d-independent-resolution-rendering/
+    int width = screenWidth;
+    int height = static_cast<int>(width / VIRTUAL_ASPECT_RATIO + 0.5f);
+
+    if (height > screenHeight) {
+        height = screenHeight;
+        width = static_cast<int>(height * VIRTUAL_ASPECT_RATIO + 0.5f);
+    }
+
+    // set up the new viewport centered in the backbuffer
+    int viewportX = (screenWidth / 2) - (width / 2);
+    int viewportY = (screenHeight / 2) - (height / 2);
+
+    SDL_Rect viewport;
+    viewport.x = viewportX;
+    viewport.y = viewportY;
+    viewport.w = width;
+    viewport.h = height;
+    SDL_RenderSetViewport(renderer, &viewport);
 
     Texture::init(renderer);
 
