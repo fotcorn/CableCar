8be912ac3d6d70b93505d851e7cf624ef802b8d2
fotcorn <fotcorn@gmail.com>
2020-05-17 19:09:38 +0100

    use older SDL2 version that is not broken on linux

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake
 class VoxelWorld(ConanFile):
   settings = 'os', 'compiler', 'build_type', 'arch'
   requires = [
-    'sdl2/2.0.12@bincrafters/stable',
+    'sdl2/2.0.9@bincrafters/stable',
     'sdl2_image/2.0.5@bincrafters/stable',
   ]
   generators = 'cmake',
